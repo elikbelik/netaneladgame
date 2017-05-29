92651e7fee21fea80ae367f8aa672f4ad6eddd26
Netanel Sananes <netanelile@gmail.com>
2017-05-29 23:49:06 +0300

    Sounds

--- app/src/main/java/com/netanelad/netaneladgame/Explosion.java
+++ app/src/main/java/com/netanelad/netaneladgame/Explosion.java
@@ -1,19 +1,25 @@
 package com.netanelad.netaneladgame;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.media.MediaPlayer;
 
 public class Explosion extends AnimatedGameObject {
     private static final int IMAGES_PER_ROW = 5;
-
-    public Explosion (Bitmap res, int x, int y, int w, int h, int numFrames) {
+    private Context context;
+    public Explosion (Context context, Bitmap res, int x, int y, int w, int h, int numFrames) {
         super(x,y,0,0,w,h,res,numFrames,IMAGES_PER_ROW);
+        this.context = context;
+        //MediaPlayer.create( getApplicationContext(), R.raw.explosion).start();
     }
 
     @Override
     public void draw (Canvas canvas) {
         if (!animation.playedOnce()) {
             super.draw(canvas);
+            playSound();
+
         }
     }
 
@@ -21,6 +27,8 @@ public class Explosion extends AnimatedGameObject {
     public void update () {
         if (!animation.playedOnce()) {
             super.update();
+
+
         }
     }
 
@@ -28,4 +36,13 @@ public class Explosion extends AnimatedGameObject {
     public boolean isColidable() {
         return false;
     }
+
+    public void playSound(){
+        MediaPlayer mediaPlayer;
+        mediaPlayer = MediaPlayer.create(context, R.raw.explosion);
+        mediaPlayer.start();
+
+    }
+
+
 }
