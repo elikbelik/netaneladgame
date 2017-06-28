54c4067d39ce3ba9db171dc1e4378eb2cff190a7
elikbelik <eladon@outlook.co.il>
2017-06-28 23:08:09 +0300

    Gitignore

--- app/src/main/java/com/netanelad/netaneladgame/Background.java
+++ app/src/main/java/com/netanelad/netaneladgame/Background.java
@@ -5,17 +5,33 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 
 public class Background {
 
+    final static int[] POSSIBLE_BACKGROUNDS = {R.drawable.bg1, R.drawable.bg2, R.drawable.bg3};
     private Bitmap image;
+    private ArrayList<Bitmap> backImages;
     private int x, y, dx;
+    private int currentBackgroundIndex;
+    private Random rand = new Random();
+    private long backStartTime;
+
 
     public Background(Context context) {
-        image = BitmapFactory.decodeResource(context.getResources(), R.drawable.grassbg1);
+        image = BitmapFactory.decodeResource(context.getResources(), R.drawable.background);
         dx = GamePanel.MOVESPEED;
         x = 0;
         y = 0;
+        currentBackgroundIndex = rand.nextInt(POSSIBLE_BACKGROUNDS.length);
+
+        // Init the background images
+        backImages = new ArrayList<Bitmap>();
+        for (int back : POSSIBLE_BACKGROUNDS ) {
+            backImages.add(BitmapFactory.decodeResource(context.getResources(), back));
+        }
     }
 
     public void update () {
@@ -26,6 +42,7 @@ public class Background {
     }
 
     public void draw (Canvas canvas) {
+        canvas.drawBitmap(backImages.get(currentBackgroundIndex), 0, 0, null);
         canvas.drawBitmap(image, x, y, null);
         if (x <0) {
             canvas.drawBitmap(image, x +GamePanel.WIDTH, y, null);
